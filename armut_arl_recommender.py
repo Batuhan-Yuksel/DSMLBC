122653c8c68b59a51384bf639a02da65b1dc02fc
Batuhan-Yuksel <124154398+Batuhan-Yuksel@users.noreply.github.com>
2023-06-11 18:07:35 +0300

    association_rule_based_recommender_system.py
    Association Rule Based Recommender System

--- armut_arl_recommender.py
+++ armut_arl_recommender.py
@@ -1,27 +1,27 @@
 ##################################
-# # # # # BATUHAN YÜKSEL # # # # #
+# # # # # BATUHAN YÃœKSEL # # # # #
 ##################################
 
 # Association Rule Based Recommender System
 
-# Ýþ Problemi
-# Türkiye’nin en büyük online hizmet platformu olan Armut, hizmet verenler ile hizmet almak isteyenleri buluþturmaktadýr.
-# Bilgisayarýn veya akýllý telefonunun üzerinden birkaç dokunuþla temizlik, tadilat, nakliyat gibi hizmetlere kolayca ulaþýlmasýný saðlamaktadýr.
-# Hizmet alan kullanýcýlarý ve bu kullanýcýlarýn almýþ olduklarý servis ve kategorileri içeren veri setini kullanarak
-# Association Rule Learning ile ürün tavsiye sistemi oluþturulmak istenmektedir.
+# Ä°ÅŸ Problemi
+# TÃ¼rkiyeâ€™nin en bÃ¼yÃ¼k online hizmet platformu olan Armut, hizmet verenler ile hizmet almak isteyenleri buluÅŸturmaktadÄ±r.
+# BilgisayarÄ±n veya akÄ±llÄ± telefonunun Ã¼zerinden birkaÃ§ dokunuÅŸla temizlik, tadilat, nakliyat gibi hizmetlere kolayca ulaÅŸÄ±lmasÄ±nÄ± saÄŸlamaktadÄ±r.
+# Hizmet alan kullanÄ±cÄ±larÄ± ve bu kullanÄ±cÄ±larÄ±n almÄ±ÅŸ olduklarÄ± servis ve kategorileri iÃ§eren veri setini kullanarak
+# Association Rule Learning ile Ã¼rÃ¼n tavsiye sistemi oluÅŸturulmak istenmektedir.
 
 # Veri Seti Hikayesi
-# Veri seti müþterilerin aldýklarý servislerden ve bu servislerin kategorilerinden oluþmaktadýr. Alýnan her hizmetin tarih ve saat
-# bilgisini içermektedir.
-# UserId: Müþteri numarasý
+# Veri seti mÃ¼ÅŸterilerin aldÄ±klarÄ± servislerden ve bu servislerin kategorilerinden oluÅŸmaktadÄ±r. AlÄ±nan her hizmetin tarih ve saat
+# bilgisini iÃ§ermektedir.
+# UserId: MÃ¼ÅŸteri numarasÄ±
 # ServiceId:
-# Her kategoriye ait anonimleþtirilmiþ servislerdir. (Örnek : Temizlik kategorisi altýnda koltuk yýkama servisi)
-# Bir ServiceId farklý kategoriler altýnda bulanabilir ve farklý kategoriler altýnda farklý servisleri ifade eder.
-# (Örnek: CategoryId’si 7 ServiceId’si 4 olan hizmet petek temizliði iken CategoryId’si 2 ServiceId’si 4 olan hizmet mobilya montaj)
-# CategoryId: Anonimleþtirilmiþ kategorilerdir. (Örnek : Temizlik, nakliyat, tadilat kategorisi)
-# CreateDate: Hizmetin satýn alýndýðý tarih
+# Her kategoriye ait anonimleÅŸtirilmiÅŸ servislerdir. (Ã–rnek : Temizlik kategorisi altÄ±nda koltuk yÄ±kama servisi)
+# Bir ServiceId farklÄ± kategoriler altÄ±nda bulanabilir ve farklÄ± kategoriler altÄ±nda farklÄ± servisleri ifade eder.
+# (Ã–rnek: CategoryIdâ€™si 7 ServiceIdâ€™si 4 olan hizmet petek temizliÄŸi iken CategoryIdâ€™si 2 ServiceIdâ€™si 4 olan hizmet mobilya montaj)
+# CategoryId: AnonimleÅŸtirilmiÅŸ kategorilerdir. (Ã–rnek : Temizlik, nakliyat, tadilat kategorisi)
+# CreateDate: Hizmetin satÄ±n alÄ±ndÄ±ÄŸÄ± tarih
 
-# Proje Görevleri
+
 import numpy as np
 import pandas as pd
 import datetime
@@ -30,24 +30,13 @@ pd.set_option('display.width', 500)
 pd.set_option('display.expand_frame_repr', False)
 from mlxtend.frequent_patterns import apriori, association_rules
 
-# GÖREV 1: Veriyi Hazýrlama
-
-# Adým 1: armut_data.csv dosyasýný okutunuz.
-
 df_ = pd.read_csv(r"C:\Users\Batuhan\Desktop\armut_data.csv")
 df = df_.copy()
-# Adým 2: ServisID her bir CategoryID özelinde farklý bir hizmeti temsil etmektedir. ServiceID ve CategoryID’yi "_" ile birleþtirerek ,
-# bu hizmetleri temsil edecek yeni bir deðiþken oluþturunuz.
 
 df.head()
 df.info()
 df["Hizmet"] = df["ServiceId"].astype("str") + "_" + df["CategoryId"].astype("str")
 
-# Adým 3: Veri seti hizmetlerin alýndýðý tarih ve saatten oluþmaktadýr, herhangi bir sepet tanýmý (fatura vb. ) bulunmamaktadýr. Association Rule
-# Learning uygulayabilmek için bir sepet (fatura vb.) tanýmý oluþturulmasý gerekmektedir. Burada sepet tanýmý her bir müþterinin aylýk aldýðý
-# hizmetlerdir. Örneðin; 7256 id'li müþteri 2017'in 8.ayýnda aldýðý 9_4, 46_4 hizmetleri bir sepeti; 2017’in 10.ayýnda aldýðý 9_4, 38_4 hizmetleri
-# baþka bir sepeti ifade etmektedir. Sepetleri unique bir ID ile tanýmlanmasý gerekmektedir. Bunun için öncelikle sadece yýl ve ay içeren yeni bir
-# date deðiþkeni oluþturunuz. UserID ve yeni oluþturduðunuz date deðiþkenini "_" ile birleþtirirek ID adýnda yeni bir deðiþkene atayýnýz.
 
 import datetime
 
@@ -55,9 +44,9 @@ df["CreateDate"] = pd.to_datetime(df["CreateDate"], format="%Y-%m-%d")
 #df["CreateDate"] = df["CreateDate"].dt.strftime("%Y-%m")
 df["SepetId"] = df["UserId"].astype("str") + "_" + df["CreateDate"].dt.strftime("%Y-%m")
 
-# GÖREV 2: Birliktelik Kurallarý Üretiniz ve Öneride bulununuz
+# GÃ–REV 2: Birliktelik KurallarÄ± Ãœretiniz ve Ã–neride bulununuz
 
-# Adým 1: Aþaðýdaki gibi sepet, hizmet pivot table’i oluþturunuz.
+# AdÄ±m 1: AÅŸaÄŸÄ±daki gibi sepet, hizmet pivot tableâ€™i oluÅŸturunuz.
 
 pivot_df = df.pivot_table(index="SepetId", columns="Hizmet").fillna(0). \
     droplevel(0, axis=1).applymap(lambda x: 1 if x > 1 else 0)
@@ -77,14 +66,11 @@ pivot_df = df.groupby(['SepetId', 'Hizmet']). \
 # 10000_2017-08 0 0 0 0 0 0..
 df[df["UserId"] == 0]
 
-# Adým 2: Birliktelik kurallarýný oluþturunuz.
-
 frequent_itemsets = apriori(pivot_df,
                             min_support=0.01,
                             use_colnames=True)
-# support deðeri min 0.01 olan tüm birliktelikleri bulduk.
-# bu bulunanlar her bir itemin olaslýðýdýr.
-frequent_itemsets.sort_values("support", ascending=False) # sýraladýk
+
+frequent_itemsets.sort_values("support", ascending=False) # sÄ±raladÄ±k
 
 rules = association_rules(frequent_itemsets,
                           metric="support",
@@ -92,9 +78,6 @@ rules = association_rules(frequent_itemsets,
 
 rules[(rules["support"]>0.01) & (rules["confidence"]>0.01) & (rules["lift"]>1)]. \
 sort_values("confidence", ascending=False)
-
-# Adým 3: arl_recommender fonksiyonunu kullanarak en son 2_0 hizmetini alan bir kullanýcýya hizmet önerisinde bulununuz.
-
 
 def arl_recommender(rules_df, hizmet, rec_count=1):
     sorted_rules = rules_df.sort_values("lift", ascending=False)
